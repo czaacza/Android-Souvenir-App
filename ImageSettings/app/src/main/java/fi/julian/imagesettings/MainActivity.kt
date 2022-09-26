70ccdcd5bb3daf7f4cfaf97d9b4b2e3647ed50cf
JulianMarc <julianmarcosoliveres@gmail.com>
2022-09-26 18:35:24 +0300

    View Almost done
    The buttons is the only thing left to done in the view

--- ImageSettings/app/src/main/java/fi/julian/imagesettings/MainActivity.kt
+++ ImageSettings/app/src/main/java/fi/julian/imagesettings/MainActivity.kt
@@ -3,14 +3,18 @@ package fi.julian.imagesettings
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import fi.julian.imagesettings.ui.theme.ImageSettingsTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +26,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    showMainView()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun showMainView(){
+        var txtTitle = remember { mutableStateOf("")}
+        var txtDescription = remember { mutableStateOf("")}
+        var txtLocation = remember { mutableStateOf("")}
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                //Image()
+                Text("Here is supposed to be the Image", modifier = Modifier.align(Alignment.CenterHorizontally))
+                Spacer(modifier = Modifier.size(40.dp))
+            }
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Column() {
+                    Text(stringResource(R.string.title))
+                    Spacer(modifier = Modifier.size(55.dp))
+                    Text(stringResource(R.string.description))
+                    Spacer(modifier = Modifier.size(53.dp))
+                    Text(stringResource(R.string.location))
+                }
+                Column() {
+                    TextField(txtTitle.value, onValueChange = {txtTitle.value = it})
+                    Spacer(modifier = Modifier.size(20.dp))
+                    TextField(txtDescription.value, onValueChange = {txtDescription.value = it})
+                    Spacer(modifier = Modifier.size(20.dp))
+                    TextField(txtLocation.value, onValueChange = {txtLocation.value = it})
+                }
+            }
+            Row(){
+                Button(onClick = { /*TODO*/ }) {
+                    Text(stringResource(R.string.cancel))
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(stringResource(R.string.done))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ImageSettingsTheme {
-        Greeting("Android")
-    }
-}
