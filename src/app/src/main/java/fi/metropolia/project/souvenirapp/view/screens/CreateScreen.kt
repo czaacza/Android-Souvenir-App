74f1b8c635fdd406fb516a37ebed54a2e4e937cb
JulianMarc <julianmarcosoliveres@gmail.com>
2022-10-07 14:52:22 +0300

    Try of creating current location
    The things I changed dosn't work

--- src/app/src/main/java/fi/metropolia/project/souvenirapp/view/screens/CreateScreen.kt
+++ src/app/src/main/java/fi/metropolia/project/souvenirapp/view/screens/CreateScreen.kt
@@ -1,5 +1,10 @@
 package fi.metropolia.project.souvenirapp.view.screens
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -15,14 +20,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
 import fi.metropolia.project.souvenirapp.R
 import fi.metropolia.project.souvenirapp.viewmodel.CameraViewModel
 import fi.metropolia.project.souvenirapp.viewmodel.LightSensorViewModel
 import fi.metropolia.project.souvenirapp.viewmodel.MemoryDatabaseViewModel
+import fi.metropolia.project.souvenirapp.view.activities.MainActivity
 
 
+@SuppressLint("MissingPermission")
 @Composable
 fun CreateScreen(
     memoryDatabaseViewModel: MemoryDatabaseViewModel,
@@ -32,6 +45,17 @@ fun CreateScreen(
     val txtTitle = remember { mutableStateOf("") }
     val txtDescription = remember { mutableStateOf("") }
     val txtLocation = remember { mutableStateOf("") }
+
+    val cts = CancellationTokenSource()
+    var fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity())
+    /*if (ActivityCompat.checkSelfPermission(LocalContext.current, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(LocalContext.current, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+    ) {
+        Log.d("DBG", "No location access")
+        ActivityCompat.requestPermissions(MainActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION),1)
+    }*/
+    var location = fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_BALANCED_POWER_ACCURACY, cts.token)
+
 
     val isPictureTaken = remember {
         mutableStateOf(false)
@@ -77,7 +101,8 @@ fun CreateScreen(
                 Spacer(modifier = Modifier.size(20.dp))
                 TextField(txtDescription.value, onValueChange = { txtDescription.value = it })
                 Spacer(modifier = Modifier.size(20.dp))
-                TextField(txtLocation.value, onValueChange = { txtLocation.value = it })
+                if(location != null) {Text(location.result.toString())}
+                else {cts.cancel()}
             }
         }
         Row(modifier = Modifier.fillMaxWidth()) {
